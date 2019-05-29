b74ae4af579426645d901180e4434466b4a02284
Nils Christian Ehmke <nils-christian.ehmke@bmiag.de>
2019-05-29 10:09:06 +0200

    joinfaces-extensionless-mapping-issue
    Signed-off-by: Nils Christian Ehmke <nils-christian.ehmke@bmiag.de>

---
+++ src/main/java/de/rhocas/joinfacesissue/JoinfacesIssueApplication.java
@@ -0,0 +1,13 @@
+package de.rhocas.joinfacesissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinfacesIssueApplication {
+
+	public static void main( final String[] args ) {
+		SpringApplication.run( JoinfacesIssueApplication.class, args );
+	}
+
+}
